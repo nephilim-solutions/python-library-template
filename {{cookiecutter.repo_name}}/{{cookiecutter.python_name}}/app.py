a1f79601063592e7170457120e42e23382e00b55
Anton Berezin <anton.berezin@nephilim.solutions>
2018-02-12 00:20:22 +0200

    Added a UML example to a docstring

--- {{cookiecutter.repo_name}}/{{cookiecutter.python_name}}/app.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.python_name}}/app.py
@@ -1,3 +1,14 @@
 def get_some_magic():
-    """ Returns some magic """
+    """
+    Returns some magic
+
+    .. uml::
+
+        class Item {
+            +GUID id
+            +DateTime creationTime
+            +DateTime modificationTime
+        }
+
+    """
     return "Magic!!"
