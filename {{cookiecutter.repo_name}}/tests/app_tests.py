e9eac2d8c09671374203eaf0ee9c21b4eead06bb
Anton Berezin <anton.berezin@nephilim.solutions>
2018-05-01 16:39:03 +0300

    added *.pyc to gitignore and transitioned to pytest

--- {{cookiecutter.repo_name}}/tests/app_tests.py
+++ {{cookiecutter.repo_name}}/tests/app_tests.py
@@ -1,9 +1,5 @@
-import unittest
-
 from {{cookiecutter.python_name}}.app import get_some_magic
 
 
-class MagicTest(unittest.TestCase):
-
-    def test_the_magic(self):
-        self.assertEqual("Magic!!", get_some_magic())
+def test_the_magic(self):
+    assert get_some_magic() == "Magic!!"
